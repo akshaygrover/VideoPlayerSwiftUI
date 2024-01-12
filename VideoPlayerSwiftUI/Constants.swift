5b0598f4135bfa6ba86b5a9880d43eaab917e61f
Akshay Grover <akshaygrover588@gmail.com>
2024-01-12 14:32:37 -0500

    Created constant file to store videos base url

---
+++ VideoPlayerSwiftUI/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by Akshay Grover on 2024-01-11.
+//
+
+import Foundation
+
+struct Constants {
+    struct URLs {
+        static let videos = URL(string: "http://localhost:4000/videos")
+    }
+}
